eec2c55a700c2e4810fc2cee9e28db0c6cdd76ed
Pankaj <pankaj.ky3007@gmail.com>
2023-10-28 09:30:55 +0530

    courseShedule1

---
+++ Graph/03_TopoSort/04_courseShedule1.cpp
@@ -0,0 +1,48 @@
+//207. There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //making an adjacency list
+	    vector<int> adj[numCourses];
+	    for(auto it: prerequisites){
+	        adj[it[1]].push_back(it[0]);
+	    }
+	    
+	    // storing number of incoming edges
+        vector<int> indegree(numCourses,0);
+	    for(int i=0;i<numCourses;i++){
+	        for(auto it: adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    for(int i=0;i<numCourses;i++){
+	        if(indegree[i]==0)
+	            q.push(i);
+	    }
+	    
+	   vector<int> topo;
+	   while(!q.empty()){
+	       int node=q.front();
+	       q.pop();
+	       topo.push_back(node);
+	       
+	       for(auto it: adj[node]){
+	           indegree[it]--;
+	           if(indegree[it]==0)
+	            q.push(it);
+	       }
+	   }
+	   
+	   if(topo.size()==numCourses)
+	     return true;
+	   return false;
+    }
+};
