23d72ea4ce0d8dd47fe36ae25a9edbe9f7ee3f44
Pankaj <pankaj.ky3007@gmail.com>
2023-12-16 07:59:04 +0530

    valid anagram

---
+++ 00_LeetCode daily Challenge/12_December/16_Dec.cpp
@@ -0,0 +1,13 @@
+//242. Valid Anagram
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
